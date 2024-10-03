1574bf3db49d1a914f67dddcac6802821ccfe997
sion-1122 <sionsaiyan.com>
2024-10-03 05:11:37 -0700

    first

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center min-h-screen">
+    <h1 className="font-bold text-8xl text-center text-red-500 underline">Hello World</h1></div>
+  );
+}
